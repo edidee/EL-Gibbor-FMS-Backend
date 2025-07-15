f2bca2e02319f9f2d7ce1a86aec3623dc47af852
adedayotoheeb <adedayotoheeb@gmail.com>
2025-07-15 22:50:48 +0100

    Added auth, student and session resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { StudentModule } from './student/student.module';
+import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, StudentModule, SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
